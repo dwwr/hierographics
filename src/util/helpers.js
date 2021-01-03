4de48919d7d1f78f23de4e6f7250cfebfafa7041
dwwr <dwwarnerreyes@gmail.com>
2021-01-02 16:39:49 -0800

    amplify deploy

---
+++ src/util/helpers.js
@@ -0,0 +1,11 @@
+function addPlayer(self, playerInfo) {
+  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    self.ship.setTint(0x0000ff);
+  } else {
+    self.ship.setTint(0xff0000);
+  }
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
+}
